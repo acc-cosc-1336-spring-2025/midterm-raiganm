306d986cf4490a546bf78a62c36ad4939617416d
raiganm <raigan.magallan@g.austincc.edu>
2025-03-15 08:22:39 +0000

    question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,11 @@
 #add import
+import question_a
+
+def main():
+
+    num = input("Enter a number: ")
+    result = question_a.get_sum_of_evens(int(num))
+
+    print(result)
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,19 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def get_sum_of_evens(num):
+
+    index = 2
+    sum = 0
+
+    while(index <= num):
+        sum += index
+        index += 2
+
+    return sum
+
+
+
+
+
